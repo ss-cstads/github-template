6105031cf7367f3c05925df0250150527d1804c7
Alex Orozco <alexorozco@ifsul.edu.br>
2026-04-06 16:18:29 -0300

    modified:   exemplos/fullstack/frontend/src/main.ts

--- exemplos/fullstack/frontend/src/main.ts
+++ exemplos/fullstack/frontend/src/main.ts
@@ -1,13 +1,15 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideHttpClient, withInterceptors, withFetch } from '@angular/common/http';
+import { provideZoneChangeDetection } from '@angular/core';
 import { routes } from './app/app.routes';
 import { authInterceptor } from './app/services/auth.interceptor';
 
 bootstrapApplication(AppComponent, {
   providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(withInterceptors([authInterceptor]))
+    provideHttpClient(withFetch(), withInterceptors([authInterceptor]))
   ]
 });
