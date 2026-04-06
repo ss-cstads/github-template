374e82eb1c4236cd75f0d52cf1ae2803adc0078f
Alex Orozco <alexorozco@ifsul.edu.br>
2026-04-06 16:47:46 -0300

    modified:   exemplos/fullstack/frontend/angular.json 	modified:   exemplos/fullstack/frontend/src/main.ts

--- exemplos/fullstack/frontend/src/main.ts
+++ exemplos/fullstack/frontend/src/main.ts
@@ -2,14 +2,11 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { importProvidersFrom } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { routes } from './app/app.routes';
 import { authInterceptor } from './app/services/auth.interceptor';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    importProvidersFrom(BrowserModule),
     provideRouter(routes),
     provideHttpClient(withInterceptors([authInterceptor]))
   ]
